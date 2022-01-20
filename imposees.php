2344d8b5698a6f9912db21474356b0fa5b78004b
chupinal <alexischup@gmail.com>
2022-01-20 17:01:40 +0100

    Add

---
+++ imposees.php
@@ -0,0 +1,15 @@
+<?php 
+    include_once("./requetes.php");
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Requêtes imposées</title>
+</head>
+<body>
+    
+</body>
+</html>
